c423821fb1a5248b625c9c07835d7519fa142733
Henrique Pontes <126187491+Pontessxx@users.noreply.github.com>
2023-11-21 13:57:03 -0300

    Update index.jsx
    logged

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,12 +7,17 @@ const index = () => {
     const showNavbar = () => {
         setIsNavOpen(!isNavOpen);
     };
+    const handleGoToRegister = () => {
+        showNavbar
+        // Redirecionar para a tela de registro
+        window.location.href = '/register'; // ou use o react-router-dom para navegação programática
+      };
   return (
     <header>
         <h3>HEALTH ID</h3>
         <nav className={isNavOpen ? "responsive_nav" : ""}>
             <Link to="/"  onClick={showNavbar}>Home</Link>
-            <Link to="/register"  onClick={showNavbar}>Registrar</Link>
+            <Link to="/register"  onClick={handleGoToRegister}>Registrar</Link>
             <button
                 className="nav-btn nav-close-btn"
                 onClick={showNavbar}
